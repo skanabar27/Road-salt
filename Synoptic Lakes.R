7f7da894bf682b7f5be992942ab18da5e119b110
Sahana Kanabar <sahana.kanabar@smu.ca>
2021-01-15 12:33:50 -0400

    Histograms for conductivity
    Checked normality for conductivity data each year. There is a general trend in increased conductivity over the years.

--- Synoptic Lakes.R
+++ Synoptic Lakes.R
@@ -13,3 +13,38 @@ library(tidyr)
 lake <- read.csv("Synoptic salt.csv")
 
 
+# plot change in conductivity over time
+lake_conductivity <- lake %>%
+  select("Name", "Cond.1980", "Cond.1991", "Sp_Cond.2000")
+
+# converting factors to class numeric
+lake_conductivity$Cond.1980 <- as.numeric(lake_conductivity$Cond.1980)
+lake_conductivity$Cond.1991 <- as.numeric(lake_conductivity$Cond.1991)
+lake_conductivity$Sp_Cond.2000 <- as.numeric(lake_conductivity$Sp_Cond.2000)
+
+#Histogram of conductivity each round of sampling. Normal distribution?
+hist(lake_conductivity$Cond.1980,
+     xlab = "Conductivity",
+     main = "Histogram of Lake Conductivity in 1980",
+     breaks = 15)
+
+hist(lake_conductivity$Cond.1991,
+     xlab = "Conductivity",
+     main = "Histogram of Lake Conductivity in 1991",
+     breaks = 15)
+
+hist(lake_conductivity$Sp_Cond.2000,
+     xlab = "Conductivity",
+     main = "Histogram of Lake Conductivity in 2000",
+     breaks = 15)
+
+# all values change over time
+ggplot(lake_conductivity) +
+  geom_line(aes(x = Year))
+
+
+
+# average change over time
+
+
+
