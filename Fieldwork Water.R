00c324a02e10eb7cd74d0d44be5cf435fd3cf684
Sahana Kanabar <sahana.kanabar@smu.ca>
2021-02-04 15:49:39 -0400

    More significance testing
    Synoptic section is likely done

--- Fieldwork Water.R
+++ Fieldwork Water.R
@@ -11,15 +11,16 @@ Water <- read.csv("Water.csv")
 
 # close proximity
 Water_close <- Water %>%
-  filter(Proximity == "close")
+  filter(Proximity == "Close")
 
 # far proximity
 Water_far <- Water %>%
-  filter(Proximity == "far")
+  filter(Proximity == "Far")
 
 # plot showing conductivity of each lake
-ggplot(data = Water, aes(x = Site, y = C, color = Proximity)) +
-  geom_bar(alpha = 0.5, size = 2) +
+png("fld water cond.png", units="mm", width=147, height=100, res=300)
+ggplot(data = Water, aes(x = Site, y = C, fill = Proximity)) +
+  geom_bar(stat = "identity", position = position_dodge()) +
   theme_classic() +
   labs(x = "\n Lake",
        y = "Conductivity (µS/cm)\n",
@@ -27,16 +28,28 @@ ggplot(data = Water, aes(x = Site, y = C, color = Proximity)) +
   theme(plot.title = element_text(hjust = 0.5),
         axis.text.x = element_text(size = 10),
         legend.position = c(0.9, 0.7)) +
-  scale_color_manual(values = c("mediumvioletred", "midnightblue"))
-                                                                        #use bar plot instead
+  scale_fill_grey(start = 0.25, end = 0.75) +
+  theme(plot.title = element_text(hjust = 0.5))+
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.title.x = element_text(size = 12),
+        axis.title.y = element_text(size = 12),
+        axis.text.x = element_text(size = 10),
+        axis.text.y = element_text(size = 10))
+dev.off()
+
 # t-test for conductivity
 
 
 
 
+
+
+
+
+
 # try with SPC
-ggplot(data = Water, aes(x = Site, y = SPC, color = Proximity)) +
-  geom_point(alpha = 0.5, size = 2) +
+ggplot(data = Water, aes(x = Site, y = SPC, fill = Proximity)) +
+  geom_bar(stat = "identity", position = position_dodge()) +
   theme_classic() +
   labs(x = "\n Lake",
        y = "Specific Conductivity (µS/cm)\n",
@@ -44,13 +57,13 @@ ggplot(data = Water, aes(x = Site, y = SPC, color = Proximity)) +
   theme(plot.title = element_text(hjust = 0.5),
         axis.text.x = element_text(size = 10),
         legend.position = c(0.9, 0.7)) +
-  scale_color_manual(values = c("mediumvioletred", "midnightblue"))
+  scale_fill_grey(start = 0.25, end = 0.75)
 #same same, just change in y-axis
 
 
 # TDS
-ggplot(data = Water, aes(x = Site, y = TDS, color = Proximity)) +
-  geom_point(alpha = 0.5, size = 2) +
+ggplot(data = Water, aes(x = Site, y = TDS, fill = Proximity)) +
+  geom_bar(stat = "identity", position = position_dodge()) +
   theme_classic() +
   labs(x = "\n Lake",
        y = "TDS (mg/L)\n",
@@ -58,12 +71,12 @@ ggplot(data = Water, aes(x = Site, y = TDS, color = Proximity)) +
   theme(plot.title = element_text(hjust = 0.5),
         axis.text.x = element_text(size = 10),
         legend.position = c(0.9, 0.7)) +
-  scale_color_manual(values = c("mediumvioletred", "midnightblue"))
+  scale_fill_grey(start = 0.25, end = 0.75)
 
 
 # SAL
-ggplot(data = Water, aes(x = Site, y = SAL, color = Proximity)) +
-  geom_point(alpha = 0.5, size = 2) +
+ggplot(data = Water, aes(x = Site, y = SAL, fill = Proximity)) +
+  geom_bar(stat = "identity", position = position_dodge()) +
   theme_classic() +
   labs(x = "\n Lake",
        y = "SAL (ppt)\n",
@@ -71,19 +84,20 @@ ggplot(data = Water, aes(x = Site, y = SAL, color = Proximity)) +
   theme(plot.title = element_text(hjust = 0.5),
         axis.text.x = element_text(size = 10),
         legend.position = c(0.9, 0.7)) +
-  scale_color_manual(values = c("mediumvioletred", "midnightblue"))
+  scale_fill_grey(start = 0.25, end = 0.75)
 
 
 # pH
-ggplot(data = Water, aes(x = Site, y = pH, color = Proximity)) +
-  geom_point(alpha = 0.5, size = 2) +
+ggplot(data = Water, aes(x = Site, y = pH, fill = Proximity)) +
+  geom_bar(stat = "identity", position = position_dodge()) +
   theme_classic() +
   labs(x = "\n Lake",
        y = "pH\n",
        title = "pH of HRM lakes by proximity to road salt application") +
   theme(plot.title = element_text(hjust = 0.5),
         axis.text.x = element_text(size = 10),
-        legend.position = c(0.9, 0.85)) +
-  scale_color_manual(values = c("mediumvioletred", "midnightblue"))
+        legend.position = c(0.95, 0.9)) +
+  scale_fill_grey(start = 0.25, end = 0.75)
+# tends to be lower pH on far side
 
 
--- Halifax Water data.R
+++ Halifax Water data.R
@@ -59,15 +59,23 @@ LLG_cond_year_count <- LLG_Sites %>%
 # very unequal....
 
 # without sd
+png("hw cond year.png", units="mm", width=147, height=100, res=300)
 ggplot(LLG_cond_year) +
   geom_line(aes(x = Year, y = Mean_Conductivity)) +
   theme_classic() +
   labs(x = "\n Year",
        y = "Mean Conductivity (µS/cm)\n",
        title = "Mean Conductivity in Lemont Lake over time") +
-  theme(plot.title = element_text(hjust = 0.5))
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.title.x = element_text(size = 12),
+        axis.title.y = element_text(size = 12),
+        axis.text.x = element_text(size = 10),
+        axis.text.y = element_text(size = 10))
+dev.off()
 
 # with sd
+png("hw cond year sd.png", units="mm", width=147, height=100, res=300)
 ggplot(LLG_cond_year) +
   geom_line(aes(x = Year, y = Mean_Conductivity)) +
   theme_classic() +
@@ -77,24 +85,22 @@ ggplot(LLG_cond_year) +
   theme(plot.title = element_text(hjust = 0.5)) +
   geom_errorbar(mapping = aes(x = Year,
                               ymin = Mean_Conductivity-SD_Conductivity,
-                              ymax = Mean_Conductivity+SD_Conductivity), width = 0.2)
+                              ymax = Mean_Conductivity+SD_Conductivity), width = 0.2) +
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.title.x = element_text(size = 12),
+        axis.title.y = element_text(size = 12),
+        axis.text.x = element_text(size = 10),
+        axis.text.y = element_text(size = 10))
+dev.off()
+
 
 
 # conductivity by month
+LLG_Sites$Month <- factor(LLG_Sites$Month, levels = month.abb)
+
 LLG_cond_month <- LLG_Sites %>%
   dplyr::group_by(Month) %>%
   dplyr::summarise(Mean_Conductivity = mean(Cond), SD_Conductivity = sd(Cond)) 
-
-                        #LLG_cond_month$Month <- as.numeric(LLG_cond_month$Month)
-
-LLG_cond_month <- LLG_cond_month %>%
-  arrange(Month = c("January", "February", "March", "April", "May", "June", "July", 
-                    "August", "September", "October", "November", "December"))
-
-LLG_cond_month <- LLG_cond_month %>%
-  mutate(Month = factor(as.character(Month),
-                        levels = c(1:12),
-                        labels = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")))
 
 # count per month
 LLG_cond_month_count <- LLG_Sites %>%
@@ -103,15 +109,23 @@ LLG_cond_month_count <- LLG_Sites %>%
 # ~20-32 samples, except Feb only has 9
 
 # without sd
+png("hw cond month.png", units="mm", width=147, height=100, res=300)
 ggplot(LLG_cond_month) +
   geom_line(aes(x = Month, y = Mean_Conductivity, group = 1)) +
   theme_classic() +
   labs(x = "\n Month",
        y = "Mean Conductivity (µS/cm)\n",
        title = "Mean Conductivity in Lemont Lake per month") +
-  theme(plot.title = element_text(hjust = 0.5))
+  theme(plot.title = element_text(hjust = 0.5))+
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.title.x = element_text(size = 12),
+        axis.title.y = element_text(size = 12),
+        axis.text.x = element_text(size = 10),
+        axis.text.y = element_text(size = 10))
+dev.off()
 
 # with sd
+png("hw cond month sd.png", units="mm", width=147, height=100, res=300)
 ggplot(LLG_cond_month) +
   geom_line(aes(x = Month, y = Mean_Conductivity, group = 1)) +
   theme_classic() +
@@ -121,7 +135,146 @@ ggplot(LLG_cond_month) +
   theme(plot.title = element_text(hjust = 0.5)) +
   geom_errorbar(mapping = aes(x = Month,
                               ymin = Mean_Conductivity-SD_Conductivity,
-                              ymax = Mean_Conductivity+SD_Conductivity), width = 0.2)
-  
+                              ymax = Mean_Conductivity+SD_Conductivity), width = 0.2) +
+  theme(plot.title = element_text(hjust = 0.5))+
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.title.x = element_text(size = 12),
+        axis.title.y = element_text(size = 12),
+        axis.text.x = element_text(size = 10),
+        axis.text.y = element_text(size = 10))
+dev.off()
+
+
+
+# statistical tests
+# conductivity across LLG sites
+# identify outliers
+LLG_Sites %>% identify_outliers(Cond)
+# LLG-03 Jan2010/Mar2012/Mar2016 are outliers, but not extreme
+
+# check for Normality
+LLG_Sites %>% shapiro_test(Cond)
+# p<0.001, cannot assume normality
+
+# median and IQR (for Wilcoxon test)
+LLG_Sites %>% get_summary_stats(Cond, type = "median_iqr")
+
+bxp_LLG <- ggboxplot(
+  LLG_Sites$Cond, width = 0.5, add = c("mean", "jitter"), 
+  ylab = "Conductivity (µS/cm)", xlab = FALSE
+)
+bxp_LLG
+
+gghistogram(LLG_Sites, x = "Cond", y = "..density..", 
+            fill = "steelblue",bins = 30, add_density = TRUE)
+# distribution is not symmetrical, so will try sign test
+
+# Sign Test
+                                        # variables do not have the same length
+
+LLG_Sites %>%
+  group_by(Sample.Site.ID) %>%
+  get_summary_stats(Cond, type = "median_iqr")
+
+bxp_llg_sign <- ggpaired(LLG_Sites, x = "Sample.Site.ID", y = "Cond", 
+                       order = c("LLG-01", "LLG-02", "LLG-03"),
+                       ylab = "Conductivity (µS/cm)", xlab = "Site")
+bxp_llg_sign
+
+stat.test <- LLG_Sites  %>%
+  sign_test(Cond ~ Sample.Site.ID) %>%
+  add_significance()
+stat.test
+# there is a significant difference (p<0.001) between conductivity in 1980, 1991, and 2000
+
+
+
+# statistical tests
+# conductivity over year
+# identify outliers
+#LLG_Sites %>% identify_outliers(Cond)
+# Whimsical 1991 is an outlier, but not extreme
+
+#synoptic_conductivity_1980 %>% identify_outliers(Conductivity)
+# Frog Pond 1980, is an outlier, but not extreme
+
+# check for Normality
+#synoptic_conductivity %>% shapiro_test(Conductivity)
+# p<0.001, cannot assume normality
+
+# median and IQR (for Wilcoxon test)
+#synoptic_conductivity %>% get_summary_stats(Conductivity, type = "median_iqr")
+
+#bxp_llg_year <- ggboxplot(
+  #LLG_Sites$Cond, width = 0.5, add = c("mean", "jitter"), 
+  #ylab = "Conductivity (µS/cm)", xlab = FALSE)
+#bxp_cond
+
+#gghistogram(LLG_Sites, x = "Cond", y = "..density..", 
+            #fill = "steelblue",bins = 30, add_density = TRUE)
+# distribution is not symmetrical, so will try sign test
+
+# Sign Test
+LLG_Sites %>%
+  group_by(Year) %>%
+  get_summary_stats(Cond, type = "median_iqr")
+
+bxp_llg_year_sign <- ggpaired(LLG_Sites, x = "Year", y = "Cond", 
+                       order = c(2009:2020),
+                       ylab = "Conductivity (µS/cm)", xlab = "Year")
+bxp_llg_year_sign
+
+stat.test <- synoptic_conductivity  %>%
+  sign_test(Conductivity ~ Year) %>%
+  add_significance()
+stat.test
+# there is a significant difference (p<0.001) between conductivity in 1980, 1991, and 2000
+
+
+
+# statistical tests
+# conductivity by month
+# identify outliers
+#synoptic_conductivity %>% identify_outliers(Conductivity)
+# Whimsical 1991 is an outlier, but not extreme
+
+# check for Normality
+#synoptic_conductivity %>% shapiro_test(Conductivity)
+# p<0.001, cannot assume normality
+
+# median and IQR (for Wilcoxon test)
+#synoptic_conductivity %>% get_summary_stats(Conductivity, type = "median_iqr")
+
+#bxp_cond <- ggboxplot(
+  #synoptic_conductivity$Conductivity, width = 0.5, add = c("mean", "jitter"), 
+  #ylab = "Conductivity (µS/cm)", xlab = FALSE)
+#bxp_cond
+
+#gghistogram(synoptic_conductivity, x = "Conductivity", y = "..density..", 
+            #fill = "steelblue",bins = 30, add_density = TRUE)
+# distribution is not symmetrical, so will try sign test
+
+# Sign Test
+LLG_Sites %>%
+  group_by(Month) %>%
+  get_summary_stats(Cond, type = "median_iqr")
+
+bxp_llg_month_sign <- ggpaired(LLG_Sites, x = "Month", y = "Cond", 
+                       order = c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"),
+                       ylab = "Conductivity (µS/cm)", xlab = "Year")
+bxp_llg_month_sign
+
+stat.test <- synoptic_conductivity  %>%
+  sign_test(Conductivity ~ Year) %>%
+  add_significance()
+stat.test
+# there is a significant difference (p<0.001) between conductivity in 1980, 1991, and 2000
+
+
+
+
+
+
+
 
 
